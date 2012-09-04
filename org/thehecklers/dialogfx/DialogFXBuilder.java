cff446611ea4cc4a98041d96599d3e756ec6a075
Gerrit Grunwald <han.solo@muenster.de>
2012-09-04 19:19:47 +0200

    Added a builder class

---
+++ org/thehecklers/dialogfx/DialogFXBuilder.java
@@ -0,0 +1,91 @@
+package org.thehecklers.dialogfx;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.scene.control.ControlBuilder;
+import javafx.util.Builder;
+
+import java.util.HashMap;
+import java.util.List;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hansolo
+ * Date: 04.09.12
+ * Time: 09:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class DialogFXBuilder <B extends DialogFXBuilder<B>> extends ControlBuilder<B> implements Builder<DialogFX> {
+    private HashMap<String, Property> properties = new HashMap<>();
+
+    protected DialogFXBuilder() {}
+
+    public static DialogFXBuilder create() {
+        return new DialogFXBuilder();
+    }
+
+    public final DialogFXBuilder buttons(final List<String> LABELS) {
+        properties.put("buttonsLabels", new SimpleObjectProperty<List<String>>(LABELS));
+        return this;
+    }
+
+    public final DialogFXBuilder buttons(final List<String> LABELS, final int DEFAULT_BUTTON, final int CANCEL_BUTTON) {
+        properties.put("buttonsLabels1", new SimpleObjectProperty<List<String>>(LABELS));
+        properties.put("buttonsDefaultButton", new SimpleIntegerProperty(DEFAULT_BUTTON));
+        properties.put("buttonsCancelButton", new SimpleIntegerProperty(CANCEL_BUTTON));
+        return this;
+    }
+
+    public final DialogFXBuilder type(final DialogFX.Type TYPE) {
+        properties.put("type", new SimpleObjectProperty<DialogFX.Type>(TYPE));
+        return this;
+    }
+
+    public final DialogFXBuilder message(final String MESSAGE) {
+        properties.put("message", new SimpleStringProperty(MESSAGE));
+        return this;
+    }
+
+    public final DialogFXBuilder modal(final boolean MODAL) {
+        properties.put("modal", new SimpleBooleanProperty(MODAL));
+        return this;
+    }
+
+    public final DialogFXBuilder titleText(final String TITLE_TEXT) {
+        properties.put("titleText", new SimpleStringProperty(TITLE_TEXT));
+        return this;
+    }
+
+
+    @Override public DialogFX build() {
+        final DialogFX CONTROL = new DialogFX();
+
+        for (String key : properties.keySet()) {
+            if ("buttonsLabels".equals(key)) {
+                CONTROL.addButtons(((ObjectProperty<List<String>>) properties.get(key)).get());
+            } else if ("buttonsLabels1".equals(key)) {
+                CONTROL.addButtons(((ObjectProperty<List<String>>) properties.get(key)).get(),
+                                   ((IntegerProperty) properties.get("buttonsDefaultButton")).get(),
+                                   ((IntegerProperty) properties.get("buttonsCancelButton")).get());
+            } else if ("type".equals(key)) {
+                CONTROL.setType(((ObjectProperty<DialogFX.Type>) properties.get(key)).get());
+            } else if ("message".equals(key)) {
+                CONTROL.setMessage(((StringProperty) properties.get(key)).get());
+            } else if ("modal".equals(key)) {
+                CONTROL.setModal(((BooleanProperty) properties.get(key)).get());
+            } else if ("titleText".equals(key)) {
+                CONTROL.setTitleText(((StringProperty) properties.get(key)).get());
+            }
+        }
+
+        return CONTROL;
+    }
+}
